b26be6a04458df900dbf1de2a88c7d6fe462b53c
Aaron Snowberger <jekkilekki@gmail.com>
2015-09-15 13:51:00 +0900

    Finish project

--- NameReversal.java
+++ NameReversal.java
@@ -8,6 +8,8 @@
 // public class - the class name MUST match the file name
 // class - it can be any name
 
+import java.util.Scanner;
+
 public class NameReversal {
   
   public static void main( String[] args ) {
@@ -68,6 +70,59 @@ public class NameReversal {
       num2 = 3 * i;
       System.out.println( "num2 = " + num2 );
     }
+    
+    /*
+     * Project: Name Reversal
+     * Des: Enter your name. Using a for loop, reverse your name and print.
+     * Ex: -> Aaron Snowberger -> regrebwonS noraA
+     */
+    String name;
+    String revName = "";
+    Scanner scan = new Scanner( System.in );
+    
+    System.out.println( "\n\nEnter your name: " );
+    name = scan.nextLine();
+    
+    System.out.println( "Your name is: " + name );
+    // Use a for loop; String; substring(...); toLowerCase() (X)
+    
+    for ( int i = name.length()-1; i >= 0; i-- ) { // name = 11, index = 10
+      revName = revName + name.substring(i, i+1);
+      //revName = revName + name.charAt(i);
+    }
+    
+    System.out.println( "Your reversed name is: " + revName );
+    
+    
+    /* 
+     * for-loop contest problems
+     * 1. B
+     * 2. E
+     * 3. A Loop = 9 (p=0++ - 1), 8 (p=1++ - 2), 7 (p=2++ - 3), 7 ( 10 )
+     * 4. E
+     * 5. D
+     * 6. E
+     * 7. D Loop = j0, k0-9, count9 - j1, k0-9, count18 - j2, k0-9, count27 - j3, k0-9, count 36
+     */
+    double pi = 0;
+    for (int mi = 10; mi > 6; --mi) {
+      if(mi==7) { pi = pi+mi; }
+      else { ++pi; }
+    }
+    System.out.println(pi);
+    
+    
+    int jl, kl;
+    int count=0;
+    for ( jl=0; jl<4; jl++ ) {
+      for ( kl=0; kl<10; kl++ ) {
+        count++; // = 10, 20, 30, 40
+        System.out.println(count);
+      }
+    }
+    System.out.print(count--); // 40
+    System.out.println(count); // 39
+    
     
   } // end main method
   
